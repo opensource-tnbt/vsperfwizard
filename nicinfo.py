e7cac66cbcbdc7b7b415acc17b1de899e952261a
Sridhar K. N. Rao <sridhar.rao@spirent.com>
2019-05-25 10:19:49 +0530

    Remote NIC Info

--- nicinfo.py
+++ nicinfo.py
@@ -2,110 +2,189 @@ import sys, os, getopt, subprocess
 from os.path import exists, abspath, dirname, basename
 import json
 import re
+import paramiko
+from stat import S_ISDIR
 
 # The PCI device class for ETHERNET devices
 ETHERNET_CLASS = "0200"
 
-# global dict ethernet devices present. Dictionary indexed by PCI address.
-# Each device within this is itself a dictionary of device properties
-devices = {}
+RECV_BYTES = 4096
 
-# This is roughly compatible with check_output function in subprocess module
-# which is only available in python 2.7.
-def check_output(args, stderr=None):
-    '''Run a command and capture its output'''
-    return subprocess.Popen(args, stdout=subprocess.PIPE,
-                            stderr=stderr, universal_newlines = True).communicate()[0]
+class RemoteInfo:
+    """
+    Class to extract information from a remote system
+    """
+    def __init__(self, host, username, password):
+        """
+        Perform Initialization
+        """
+        # Dict of ethernet devices present. Dictionary indexed by PCI address.
+        # Each device within this is itself a dictionary of device properties
+        self.nic_devices = {}
 
-def get_pci_device_details(dev_id):
-    '''This function gets additional details for a PCI device'''
-    device = {}
-
-    extra_info = check_output(["lspci", "-vmmks", dev_id]).splitlines()
-
-    # parse lspci details
-    for line in extra_info:
-        if len(line) == 0:
-            continue
-        name, value = line.split("\t", 1)
-        name = name.strip(":") + "_str"
-        device[name] = value
-    # check for a unix interface name
-    sys_path = "/sys/bus/pci/devices/%s/net/" % dev_id
-    if exists(sys_path):
-        device["Interface"] = ",".join(os.listdir(sys_path))
-    else:
-        device["Interface"] = ""
-    # check if a port is used for ssh connection
-    device["Ssh_if"] = False
-    device["Active"] = ""
-
-    return device
-
-def get_nic_details():
-    '''This function populates the "devices" dictionary. The keys used are
-    the pci addresses (domain:bus:slot.func). The values are themselves
-    dictionaries - one for each NIC.'''
-    global devices
-    devinfos = []
-    # clear any old data
-    devices = {}
-    # first loop through and read details for all devices
-    # request machine readable format, with numeric IDs
-    dev = {};
-    dev_lines = check_output(["lspci", "-Dvmmn"]).splitlines()
-    for dev_line in dev_lines:
-        if (len(dev_line) == 0):
-            if dev["Class"] == ETHERNET_CLASS:
-                #convert device and vendor ids to numbers, then add to global
-                dev["Vendor"] = int(dev["Vendor"],16)
-                dev["Device"] = int(dev["Device"],16)
-                devices[dev["Slot"]] = dict(dev) # use dict to make copy of dev
+        if host is 'local':
+            self.local = True
         else:
-            #values = re.split(r'\t+', str(dev_line))
-            #name, value = dev_line.split(b'\t')
-            name, value = dev_line.split("\t", 1)
-            dev[name.rstrip(":")] = value
+            self.local = False
+            # Assuming port as 22.
+            self.port = 22
+            self.hostname = host
+            self.password = password
+            self.username = username
+            self.client = paramiko.Transport((self.hostname, self.port))
+            self.client.connect(username = self.username,
+                                password = self.password)
+            self.session = self.client.open_channel(kind = 'session')
+            self.session.get_pty()
+            self.sftp = paramiko.SFTPClient.from_transport(self.client)
 
-    # based on the basic info, get extended text details
-    for d in devices.keys():
-        # get additional info and add it to existing data
+    def sftp_exists(self, path):
+        try:
+            self.sftp.stat(path)
+            return True
+        except IOError:
+            return False
+        except FileNotFoundError:
+            return False
 
-        #devices[d] = dict(devices[d].items() +
-        #                  get_pci_device_details(d).items())
-        devices[d].update(get_pci_device_details(d).items())
-        devinfos.append(devices[d])
-    return devinfos
+    def sft_listdir(self, path):
+        files = []
+        for f in self.sftp.listdir_attr(path):
+            if not S_ISDIR(f.st_mode):
+                files.append(f.filename)
+        return files
+
+    def is_connected(self):
+        return self.client.is_active()
+
+    def new_channel(self):
+        if not self.is_connected():
+            self.client = paramiko.Transport((self.hostname, self.port))
+            self.client.connect(username = self.username,
+                                password = self.password)
+        self.session = self.client.open_channel(kind = 'session')
+
+    # This is roughly compatible with check_output function in subprocess module
+    # which is only available in python 2.7.
+    def check_output(self, args, stderr=None):
+        '''Run a command and capture its output'''
+        stdout_data = []
+        stderr_data = []
+        if self.local:
+            return subprocess.Popen(args, stdout=subprocess.PIPE,
+                                    stderr=stderr,
+                                    universal_newlines = True).communicate()[0]
+        else:
+            self.new_channel()
+            separator = ' '
+            command = separator.join(args)
+            # self.session.get_pty()
+            self.session.exec_command(command)
+            while True:
+                if self.session.recv_ready():
+                    stdout_data.append(self.session.recv(RECV_BYTES))
+                if self.session.recv_stderr_ready():
+                    stderr_data.append(self.session.recv_stderr(RECV_BYTES))
+                if self.session.exit_status_ready():
+                    break
+            if stdout_data:
+                return b"".join(stdout_data)
+            else:
+                return b"".join(stderr_data)
+
+    def get_pci_device_details(self, dev_id):
+        '''This function gets additional details for a PCI device'''
+        device = {}
+
+        extra_info = self.check_output(["/usr/sbin/lspci",
+                                        "-vmmks", dev_id]).splitlines()
+
+        # parse lspci details
+        for line in extra_info:
+            if len(line) == 0:
+                continue
+            name, value = line.decode().split("\t", 1)
+            name = name.strip(":") + "_str"
+            device[name] = value
+        # check for a unix interface name
+        sys_path = "/sys/bus/pci/devices/%s/net/" % dev_id
+        device["Interface"] = ""
+        if self.local:
+            if exists(sys_path):
+                device["Interface"] = ",".join(os.listdir(sys_path))
+        else:
+            if self.sftp_exists(sys_path):
+                device["Interface"] = ",".join(self.sft_listdir(sys_path))
+
+        # check if a port is used for ssh connection
+        device["Ssh_if"] = False
+        device["Active"] = ""
+
+        return device
+
+    def get_nic_details(self):
+        '''This function populates the "devices" dictionary. The keys used are
+        the pci addresses (domain:bus:slot.func). The values are themselves
+        dictionaries - one for each NIC.'''
+        devinfos = []
+        # first loop through and read details for all devices
+        # request machine readable format, with numeric IDs
+        dev = {};
+        dev_lines = self.check_output(["/usr/sbin/lspci", "-Dvmmn"]).splitlines()
+        for dev_line in dev_lines:
+            if (len(dev_line) == 0):
+                if dev["Class"] == ETHERNET_CLASS:
+                    #convert device and vendor ids to numbers, then add to global
+                    dev["Vendor"] = int(dev["Vendor"],16)
+                    dev["Device"] = int(dev["Device"],16)
+                    self.nic_devices[dev["Slot"]] = dict(dev) # use dict to make copy of dev
+            else:
+                values = re.split(r'\t+', str(dev_line))
+                #name, value = dev_line.split(b'\t', 1)
+                name, value = dev_line.decode().split("\t", 1)
+                dev[name.rstrip(":")] = value
+
+        # based on the basic info, get extended text details
+        for d in self.nic_devices.keys():
+            # get additional info and add it to existing data
+
+            #devices[d] = dict(devices[d].items() +
+            #                  get_pci_device_details(d).items())
+            self.nic_devices[d].update(self.get_pci_device_details(d).items())
+            devinfos.append(self.nic_devices[d])
+        return devinfos
 
 
-def dev_id_from_dev_name(dev_name):
-    '''Take a device "name" - a string passed in by user to identify a NIC
-    device, and determine the device id - i.e. the domain:bus:slot.func - for
-    it, which can then be used to index into the devices array'''
-    dev = None
-    # check if it's already a suitable index
-    if dev_name in devices:
-        return dev_name
-    # check if it's an index just missing the domain part
-    elif "0000:" + dev_name in devices:
-        return "0000:" + dev_name
-    else:
-        # check if it's an interface name, e.g. eth1
-        for d in devices.keys():
-            if dev_name in devices[d]["Interface"].split(","):
-                return devices[d]["Slot"]
-    # if nothing else matches - error
-    print ("Unknown device: %s. " \
-        "Please specify device in \"bus:slot.func\" format" % dev_name)
-    sys.exit(1)
+    def dev_id_from_dev_name(self, dev_name):
+        '''Take a device "name" - a string passed in by user to identify a NIC
+        device, and determine the device id - i.e. the domain:bus:slot.func - for
+        it, which can then be used to index into the devices array'''
+        dev = None
+        # check if it's already a suitable index
+        if dev_name in devices:
+            return dev_name
+        # check if it's an index just missing the domain part
+        elif "0000:" + dev_name in devices:
+            return "0000:" + dev_name
+        else:
+            # check if it's an interface name, e.g. eth1
+            for d in devices.keys():
+                if dev_name in devices[d]["Interface"].split(","):
+                    return devices[d]["Slot"]
+        # if nothing else matches - error
+        print ("Unknown device: %s. " \
+            "Please specify device in \"bus:slot.func\" format" % dev_name)
+        sys.exit(1)
 
 def main():
     '''program main function'''
-    global devices
-    dev_list = get_nic_details()
+    host = raw_input("Enter Host IP: ")
+    username = raw_input("Enter User Name: ")
+    pwd = raw_input("Enter Password: ")
+    rhi = RemoteInfo(host, username, pwd)
+    dev_list = rhi.get_nic_details()
     for dev in dev_list:
         print(dev["Slot"])
-        print(dev["Interface"])
 
 
 if __name__ == "__main__":
--- vsperfwiz.py
+++ vsperfwiz.py
@@ -2,67 +2,142 @@ from __future__ import print_function
 from pypsi import wizard as wiz
 from pypsi.shell import Shell
 import nicinfo
+import signal
+import sys
 
-def main():
-    shell = Shell()
-    dev_list = nicinfo.get_nic_details()
-    devices = ''
-    for dev in dev_list:
-        devices += str(dev["Interface"]) + '::' + str(dev["Slot"]) + ', '
-    print(devices)
-    wiz_main = wiz.PromptWizard(
-        name="VSPERF Common Configuration",
-        description="This configuration covers Basic inputs",
-        steps=(
-            # The list of input prompts to ask the user.
-            wiz.WizardStep(
-                # ID where the value will be stored
-                id="vswitch",
-                # Display name
-                name="Vswitch ? [OVS or VPP]",
-                # Help message
-                help=" Enter the vswitch to use - either OVS or VPP",
-                # List of validators to run on the input
-                validators=(wiz.required_validator)
-            ),
-            wiz.WizardStep(
-                id='nics',
-                name="NICs to Whitelist: " + devices,
-                help="Enter the list (separated by comma) of PCI-IDs",
-                validators=(wiz.required_validator),
-            ),
-            wiz.WizardStep(
-                id='topology',
-                name="What trafficgen to use?",
-                help="Enter the trafficgen to use - STC, Ixia, Moongen , T-Rex",
-                validators=(wiz.required_validator),
-                default="Ixia"
+class VsperfWizard():
+    """
+    Class to create wizards
+    """
+    def __init__(self):
+        self.shell = Shell()
+
+######## Support Functions ############################
+    def get_nicpcis(self):
+        self.rhi = nicinfo.RemoteInfo(self.dut_values['dutip'],
+                                      self.dut_values['dutuname'],
+                                      self.dut_values['dutpwd'])
+        dev_list = self.rhi.get_nic_details()
+        self.devices = ''
+        index = 0
+        for dev in dev_list:
+            self.devices += str("("+str(index)+")"+ " "
+                                + str(dev["Slot"]) + ', ')
+            index = index +1
+
+############# All the Wizards ##################################
+
+    def dut_wizard(self):
+        self.wiz_dut = wiz.PromptWizard(
+            name="VSPERF DUT Info Collection",
+            description="This collects DUT info",
+            steps=(
+                # The list of input prompts to ask the user.
+                wiz.WizardStep(
+                    # ID where the value will be stored
+                    id="dutip",
+                    # Display name
+                    name="Enter the IP address of the DUT [local]",
+                    # Help message
+                    help= "IP address of the DUT host",
+                    # List of validators to run on the input
+                    validators=(wiz.required_validator)
+                ),
+                wiz.WizardStep(
+                    # ID where the value will be stored
+                    id="dutuname",
+                    # Display name
+                    name="Enter the username to connect to DUT",
+                    # Help message
+                    help= "Username for DUT host",
+                    # List of validators to run on the input
+                    validators=(wiz.required_validator)
+                ),
+                wiz.WizardStep(
+                    # ID where the value will be stored
+                    id="dutpwd",
+                    # Display name
+                    name="Enter the Password to connect to DUT",
+                    # Help message
+                    help= "Password for the DUT host",
+                    # List of validators to run on the input
+                    validators=(wiz.required_validator)
+                ),
             )
         )
-    )
-    wiz_trafficconf = wiz.PromptWizard(
-        name="Traffic Configuration",
-        description="This configuration covers Traffic specifc inputs",
-        steps=(
-            wiz.WizardStep(
-                id='multistream',
-                name='Multistream preferred?',
-                help="Multistream preference - Yes or No",
-                default='No',
-                # validators=(wiz.required_validator)
-            ),
-            wiz.WizardStep(
-                id='count',
-                name='Number of flows?',
-                help="Enter the number of flows - 2 - 1,000,000",
-                default='1000',
-                # validators=(wiz.required_validator)
-            ),
+
+    def main_wizard(self):
+        # First get the nics.
+        self.get_nicpcis()
+        self.wiz_main = wiz.PromptWizard(
+            name="VSPERF Common Configuration",
+            description="This configuration covers Basic inputs",
+            steps=(
+                # The list of input prompts to ask the user.
+                wiz.WizardStep(
+                    # ID where the value will be stored
+                    id="vswitch",
+                    # Display name
+                    name="Vswitch ? [OVS or VPP]",
+                    # Help message
+                    help=" Enter the vswitch to use - either OVS or VPP",
+                    # List of validators to run on the input
+                    validators=(wiz.required_validator)
+                ),
+                wiz.WizardStep(
+                    id='nics',
+                    name="NICs to Whitelist: " + self.devices,
+                    help="Enter the list (separated by comma) of PCI-IDs",
+                    validators=(wiz.required_validator),
+                ),
+                wiz.WizardStep(
+                    id='topology',
+                    name="What trafficgen to use?",
+                    help="Enter the trafficgen to use - STC, Ixia, Moongen, T-Rex",
+                    validators=(wiz.required_validator),
+                    default="T-Rex"
+                )
+            )
         )
-    )
-    main_values = wiz_main.run(shell)
-    if main_values['vswitch'] == 'OVS':
-        wiz_vswitch = wiz.PromptWizard(
+
+    def traffic_wizard(self):
+        self.wiz_traffic = wiz.PromptWizard(
+            name="Traffic Configuration",
+            description="This configuration covers Traffic specifc inputs",
+            steps=(
+                wiz.WizardStep(
+                    id='multistream',
+                    name='Multistream preferred?',
+                    help="Multistream preference - Yes or No",
+                    default='No',
+                    # validators=(wiz.required_validator)
+                ),
+                wiz.WizardStep(
+                    id='count',
+                    name='Number of flows?',
+                    help="Enter the number of flows - 2 - 1,000,000",
+                    default='1000',
+                    # validators=(wiz.required_validator)
+                ),
+            )
+        )
+
+    def ovs_wizard(self):
+        self.wiz_ovs = wiz.PromptWizard(
+                name = "Vswitch Configuration",
+                description="Specific configurations of the virtual-Switch",
+                steps=(
+                    wiz.WizardStep(
+                        id='ovs-type',
+                        name='OVS Type? [Vanilla or DPDK]',
+                        help='Enter either Vanilla or DPDK',
+                        default='Vanilla',
+                    ),
+                )
+            )
+
+    def vpp_wizard(self):
+        self.wiz_vpp = wiz.PromptWizard(
             name = "Vswitch Configuration",
             description="Specific configurations of the virtual-Switch",
             steps=(
@@ -74,18 +149,56 @@ def main():
                 ),
             )
         )
-    elif main_values['vswitch'] == 'VPP':
-        wiz_vswitch = wiz.PromptWizard(
 
-        )
+############### All the Run Operations ######################
 
-    if wiz_vswitch:
-        vswitch_values = wiz_vswitch.run(shell)
-    traffic_values = wiz_trafficconf.run(shell)
-    nics = main_values['nics']
-    nics = nics.split(',')
-    for nic in nics:
-        print(nic)
+    def run_dutwiz(self):
+        self.dut_wizard()
+        self.dut_values = self.wiz_dut.run(self.shell)
+
+    def run_mainwiz(self):
+        self.main_wizard()
+        self.main_values = self.wiz_main.run(self.shell)
+
+    def run_vswitchwiz(self):
+        if self.main_values['vswitch'] == 'OVS':
+           self.ovs_wizard()
+           self.ovs_values = self.wiz_ovs.run(self.shell)
+        elif self.main_values['vswitch'] == 'VPP':
+            self.vpp_wizard()
+            self.vpp_values = self.wiz_vpp.run(self.shell)
+
+    def run_trafficwiz(self):
+        self.traffic_wizard()
+        self.traffic_value = self.wiz_traffic.run(self.shell)
+
+################ Prepare Configuration File ##################
+
+    def prepare_conffile(self):
+        with open("./vsperf.conf", 'w+') as ofile:
+            ofile.write("#### This file is Automatically Created")
+
+
+def signal_handler(signal, frame):
+    print("\n You interrupted, No File will be generated!")
+    sys.exit(0)
+
+
+def main():
+    try:
+        vwiz = VsperfWizard()
+        vwiz.run_dutwiz()
+        vwiz.run_mainwiz()
+        vwiz.run_vswitchwiz()
+        vwiz.run_trafficwiz()
+        vwiz.prepare_conffile()
+    except (KeyboardInterrupt, MemoryError):
+        print("Some Error Occured, No file will be generated!")
+
+    print("Thanks for using the VSPERF-WIZARD, Please look for vsperf.conf \
+          file in the current folder")
+
 
 if __name__ == "__main__":
+    signal.signal(signal.SIGINT, signal_handler)
     main()
